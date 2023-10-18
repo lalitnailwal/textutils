b5b7ea05d5c4226e4a7836844877d96c412c940a
lalitnailwal <36082917+lalitnailwal@users.noreply.github.com>
2023-10-18 20:04:44 +0530

    Fixing few more bugs in TextUtils (V-21)

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -28,10 +28,7 @@ export default function TextForm(props) {
     }
 
     const handleCopy = () => {
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
-        document.getSelection().removeAllRanges();
+        navigator.clipboard.writeText(text);
         props.showAlert("Copied to Clipboard", "success");
     }
 
@@ -62,7 +59,7 @@ export default function TextForm(props) {
 
             <div className="Container my-3" style={{color: props.mode === 'dark'?'white':'#042743'}}>
                 <h2>Your text Summary</h2>
-                <p>{text.split(" ").filter((element) => { return element.length !== 0}).length} words and {text.length} characters</p>
+                <p>{text.split(/\s+/).filter((element) => { return element.length !== 0}).length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").filter((element) => { return element.length !== 0}).length} Minutes read</p>
                 <h2>Preview</h2>
                 <p>{text.length>0 ? text : "Nothing to preview!"}</p>
